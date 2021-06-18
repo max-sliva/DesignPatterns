3210af97e1dee12d759c2de1323a35dd6b335d1d
max-sliva <persimon@inbox.ru>
2021-06-18 18:19:45 +0500

    done Builder

--- src/main/kotlin/Java/CarBuilderJava.java
+++ src/main/kotlin/Java/CarBuilderJava.java
@@ -9,5 +9,63 @@ public class CarBuilderJava {
     private String color;
     private int year;
 
+    public static class Builder {
+        private CarBuilderJava newCar;
 
+        public Builder() {
+            newCar = new CarBuilderJava();
+        }
+
+        public Builder withFirm(String firm){
+            newCar.firm = firm;
+            return this;
+        }
+
+        public Builder withModel(String model){
+            newCar.model = model;
+            return this;
+        }
+
+        public Builder withEngine(float Engine){
+            newCar.vEngine = Engine;
+            return this;
+        }
+
+        public Builder withMaxSpeed(int maxSpeed){
+            newCar.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public Builder withDoors(int doors){
+            newCar.doors = doors;
+            return this;
+        }
+
+        public Builder withColor(String color){
+            newCar.color = color;
+            return this;
+        }
+
+        public Builder withYear(int year){
+            newCar.year = year;
+            return this;
+        }
+
+        public CarBuilderJava build(){
+            return newCar;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CarBuilderJava{" +
+                "firm='" + firm + '\'' +
+                ", model='" + model + '\'' +
+                ", vEngine=" + vEngine +
+                ", maxSpeed=" + maxSpeed +
+                ", doors=" + doors +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
--- src/main/kotlin/Java/lab2_2.java
+++ src/main/kotlin/Java/lab2_2.java
@@ -3,5 +3,17 @@ package Java;
 public class lab2_2 {
     public static void main(String[] args) {
         CarSimpleJava myCar = new CarSimpleJava("Lada", "Kalina", 2.6f, 200, 4, "Белый", 2010);
+
+        CarBuilderJava myCar2 = new CarBuilderJava.Builder()
+                .withFirm("Lada")
+                .withModel("Kalina")
+                .withEngine(2.6f)
+                .withMaxSpeed(200)
+                .withDoors(4)
+                .withColor("Белый")
+                .withYear(2010)
+                .build();
+        System.out.println("car = "+myCar2);
+//todo объединить фабрику и строитель
     }
 }
