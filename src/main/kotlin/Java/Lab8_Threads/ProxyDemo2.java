348bd3869d70112b8e8af523db43d327811f6bc8
max-sliva <persimon@inbox.ru>
2021-10-04 16:32:11 +0500

    Lab8_Threads update

--- src/main/kotlin/Java/Lab8_Threads/ProxyDemo2.java
+++ src/main/kotlin/Java/Lab8_Threads/ProxyDemo2.java
@@ -1,10 +1,27 @@
 package Java.Lab8_Threads;
 
+import com.sun.javafx.logging.JFRInputEvent;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class ProxyDemo2 {
     public static void main(String[] args) {
         System.out.println("Расписание");
         DisplayTimetable2 displayTimetable = new DisplayTimetable2();
         displayTimetable.printTimetable();
-
+        createGUI();
     }
+
+    private static void createGUI() {
+        JFrame myFrame = new JFrame("Расписание");
+        setCenterTable(myFrame);
+    }
+
+    private static void setCenterTable(JFrame frame) {
+        JTable centerTable = new JTable();
+
+        frame.add(centerTable, BorderLayout.CENTER);
+    }
+
 }
